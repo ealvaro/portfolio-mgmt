1deacd81cf88a1d6beedc9ffbe8ded9f90a30527
ealvaro <alvaroescobaronline@gmail.com>
2020-05-27 23:59:12 -0400

    Added Holding Present Value

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ActiveRecord::Base
-
+  validates :symbol, presence: true
   has_many :portfolios, :through => :holdings
   has_many :holdings
 
--- test/unit/stock_test.rb
+++ test/unit/stock_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save stock without symbol" do
+    stock = Stock.new
+    assert_not stock.save, "Saved the stock without a symbol"
+  end
 end
